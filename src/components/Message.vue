34c75a280a42229ead901dc56f3ddbc9828a5316
Zerio <54480523+Z3rio@users.noreply.github.com>
2022-10-11 18:08:49 +0200

    Setup message structure

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,5 +1,61 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
 
-<template></template>
+defineProps<{
+  chat: Object;
+  user: Object;
+}>();
 
-<style scoped></style>
+const currentUser = ref({
+  username: "Test123123",
+  profilepicture:
+    "https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg",
+});
+</script>
+
+<template>
+  <div
+    class="message"
+    :style="{
+      'flex-direction': chat.isYou ? 'row-reverse' : 'row',
+    }"
+  >
+    <div
+      class="info"
+      :style="{
+        'align-items': chat.isYou ? 'flex-end' : 'flex-start',
+      }"
+    >
+      <img
+        :src="chat.isYou ? user.profilepicture : currentUser.profilepicture"
+        alt=""
+      />
+      <p>
+        {{ chat.dateSent.toLocaleDateString() }}
+        {{ chat.dateSent.getHours() }}:{{ chat.dateSent.getMinutes() }}
+      </p>
+    </div>
+    <div class="message-content">
+      <h2>{{ chat.message }}</h2>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.message {
+  padding: 10px 30px;
+
+  display: flex;
+}
+
+.info {
+  display: flex;
+  flex-direction: column;
+}
+
+img {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+}
+</style>
--- src/components/Messages.vue
+++ src/components/Messages.vue
@@ -1,7 +1,35 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+import Message from "./Message.vue";
+
+const chats = ref({
+  username: "Test",
+  profilepicture:
+    "https://i.pinimg.com/736x/65/25/a0/6525a08f1df98a2e3a545fe2ace4be47.jpg",
+  chats: [
+    {
+      message: "hi1asdfasdffasdfasdfasfadsfdasadsfafdsafds",
+      dateSent: new Date(1665503950999),
+      isYou: false,
+    },
+    {
+      message: "hi2",
+      dateSent: new Date(1665503970999),
+      isYou: true,
+    },
+  ],
+});
+</script>
 
 <template>
-  <div class="messages-container"></div>
+  <div class="messages-container">
+    <Message
+      v-for="(chat, index) in chats.chats"
+      :key="index"
+      :chat="chat"
+      :user="{ username: chats.username, profilepicture: chats.profilepicture }"
+    />
+  </div>
 </template>
 
 <style scoped>
